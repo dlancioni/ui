05207ac1e62ba4055b457cc93c0051627837bd76
David Lancioni <dlancioni@gmail.com>
2020-09-07 10:53:46 -0300

    Undo folder

--- php/include.php
+++ php/include.php
@@ -3,15 +3,15 @@
     include "exception.php";
     
     // Include classes
-    include "class/base.php";
-    include "class/db.php";
-    include "class/sqlbuilder.php";
-    include "class/util.php";
-    include "class/report.php";
-    include "class/form.php";
-    include "class/filter.php";
-    include "class/element.php";
-    include "class/menu.php";
-    include "class/tablelogic.php";
-    include "class/eventaction.php";
+    include "base.php";
+    include "db.php";
+    include "sqlbuilder.php";
+    include "util.php";
+    include "report.php";
+    include "form.php";
+    include "filter.php";
+    include "element.php";
+    include "menu.php";
+    include "tablelogic.php";
+    include "eventaction.php";
 ?>    
