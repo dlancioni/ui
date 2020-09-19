25428357cb593f60001a2254bfcab9a038983f01
David Lancioni <dlancioni@gmail.com>
2020-09-19 16:08:29 -0300

    Change index

--- index.php
+++ index.php
@@ -13,54 +13,52 @@ include "php/page.php";
 <link rel="stylesheet" href="css/w3.css">
 <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
 <style>
-html, body, h1, h2, h3, h4, h5, h6 {
-  font-family: "Arial";
-}
-
+    html, body, h1, h2, h3, h4, h5, h6 {
+    font-family: "Arial";
+    }
 </style>
-<body onKeyDown="__shortcut__(event)" onload="<?php echo $onLoadFunctions; ?>">
+<br>
+
+<body onKeyDown="__shortcut__(event)" onload="<?php echo $onLoadFunctions;?>">
 <form id="form1" name="form1" action="index.php" method="post">
 
-    <br>
-
+    <!-- Main container -->
     <div class="w3-container">
 
+        <!-- Topnav -->
         <div class="w3-row">
             <div class="w3-container w3-black">
                 <img src="img/home.png" width="50" heigth="50">
             </div>
         </div>
 
-        <div class="w3-row">
+        <!-- Main area -->
+        <div class="w3-row">       
 
+            <!-- Main menu -->
             <div class="w3-quarter">
                 <div class="w3-panel">
                     <?php echo $mainMenu; ?>
                 </div>
             </div>
 
-            <div class="w3-half">
+            <!-- Contents -->
+            <div class="w3-threequarter">
                 <div class="w3-panel">
                 <?php echo $html;?>
                 </div>            
             </div>
 
-            <div class="w3-quarter" >
-                <div class="w3-panel">
-                    <?php echo $mainMenu; ?>
-                </div>
-            </div>
-            
         </div>
     </div>
 
-
-
+    <!-- Session Info -->
     <input type="hidden" id="_TABLE_" name="_TABLE_" value="<?php echo $tableId; ?>">
     <input type="hidden" id="_FORMAT_" name="_FORMAT_" value="<?php echo $format; ?>">
     <input type="hidden" id="_EVENT_" name="_EVENT_" value="<?php echo $event; ?>">
     <input type="hidden" id="_PAGING_" name="_PAGING_" value="<?php echo $pageOffset; ?>">
 
+    <!-- JS -->
     <script src="js/filesaver.js"></script>
     <script src="js/moment.js"></script>
     <script src="js/httpservice.js"></script>
