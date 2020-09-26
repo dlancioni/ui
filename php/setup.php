b213d74239692e48d7fd7e778063d9806cefeab4
David Lancioni <dlancioni@gmail.com>
2020-09-26 12:16:36 -0300

    Create setup.php

--- php/setup.php
+++ php/setup.php
@@ -29,6 +29,7 @@
         createTable($cn);
         createTransaction($cn);
         createField($cn);
+        createDomain($cn);
 
         // Finished OK
         printl("Success :)");
@@ -236,6 +237,66 @@
         }
     }
 
+    /*
+     * Create domain
+     */
+    function createDomain($cn) {
+
+        $json = "";
+        global $tableName;
+
+        try {
+            
+            // Create sysstem
+            $tableName = "tb_domain";
+
+            // tb_table_type
+            execute($cn, '{"id_system":1,"id_group":1,"key":"1","value":"Formulário","domain":"tb_table_type"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":"2","value":"Relatório","domain":"tb_table_type"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":"3","value":"Menu","domain":"tb_table_type"}');
+            // tb_bool
+            execute($cn, '{"id_system":1,"id_group":1,"key":"1","value":"Sim","domain":"tb_bool"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":"2","value":"Não","domain":"tb_bool"}');
+            // tb_field_type
+            execute($cn, '{"id_system":1,"id_group":1,"key":1,"value":"Inteiro","domain":"tb_field_type"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":2,"value":"Decimal","domain":"tb_field_type"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":3,"value":"Texto","domain":"tb_field_type"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":4,"value":"Data","domain":"tb_field_type"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":5,"value":"Hora","domain":"tb_field_type"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":6,"value":"Area","domain":"tb_field_type"}');
+            // tb_event
+            execute($cn, '{"id_system":1,"id_group":1,"key":1,"value":"onLoad","domain":"tb_event"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":2,"value":"onClick","domain":"tb_event"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":3,"value":"onChange","domain":"tb_event"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":4,"value":"onFocus","domain":"tb_event"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":5,"value":"onBlur","domain":"tb_event"}');
+            // tb_target
+            execute($cn, '{"id_system":1,"id_group":1,"key":1,"value":"Tabela","domain":"tb_target"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":2,"value":"Formulário","domain":"tb_target"}');
+            // tb_message
+            execute($cn, '{"id_system":1,"id_group":1,"key":"A1","value":"Campo % é obrigatório","domain":"tb_message"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":"A2","value":"Data inválida informada no campo %","domain":"tb_message"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":"A3","value":"Numero inválido informada no campo %","domain":"tb_message"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":"A4","value":"Os valores para os campos % ja existem na tabela e não podem se repetir","domain":"tb_message"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":"A5","value":"Nenhuma mudança identifica no registro, alteração não realizada","domain":"tb_message"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":"A6","value":"Registro incluído com sucesso","domain":"tb_message"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":"A7","value":"Registro alterado com sucesso","domain":"tb_message"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":"A8","value":"Registro excluído com sucesso","domain":"tb_message"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":"A9","value":"Campo Tabela FK foi selecionado, entao Campo FK é obrigatório","domain":"tb_message"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":"A10","value":"Transação selecionada é do tipo menu, não é permitido adicionar campos","domain":"tb_message"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":"A11","value":"Registro pertence ao grupo Sistema, não pode ser modificado ou excluído","domain":"tb_message"}');
+            // tb_cascade
+            execute($cn, '{"id_system":1,"id_group":1,"key":"tb_field.id_table_fk","value":"id_field_fk; tb_field; id; label","domain":"tb_cascade"}');
+            execute($cn, '{"id_system":1,"id_group":1,"key":"tb_event.id_table","value":"id_field; tb_field; id; label","domain":"tb_cascade"}');            
+
+            // Success
+            printl("createDomain() OK");
+            
+        } catch (Exception $ex) {
+            printl("createDomain():" . $ex->getMessage());
+            throw $ex;
+        }
+    }
 
 
 
@@ -245,15 +306,28 @@
 
 
 
+    /*
+     * Create xyz
+     */
+    function createXYZ($cn) {
 
+        $json = "";
+        global $tableName;
 
+        try {
 
+            // Create sysstem
+            $tableName = "tb_system";
+            execute($cn, 'json here');
 
-
-
-
-
-
+            // Success
+            printl("createXYZ() OK");
+            
+        } catch (Exception $ex) {
+            printl("createXYZ():" . $ex->getMessage());
+            throw $ex;
+        }
+    }
 
     function execute($cn, $json) {
 
