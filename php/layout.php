e7d37e196facf1d0c25cda712adcb2755a7b808d
David Lancioni <dlancioni@gmail.com>
2020-09-28 22:26:25 -0300

    Add bootstrap

--- php/layout.php
+++ php/layout.php
@@ -25,8 +25,34 @@
             </div>            
           </li>          
         </ul>
-
-
-
       </div>
     </nav>
+
+
+<br>
+<br>
+
+
+
+<div class="container">
+
+    <table class="table table-sm">
+
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td style="width: 10%">1</th>
+                <td style="width: 20%">Mark</td>
+                <td style="width: 70%">Ottoooooooooooooooo</td>
+            </tr>
+        </tbody>
+    </table>
+
+</div>
