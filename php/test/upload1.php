4dce32386d3866d1159ca1cfe3ef7ea8d3b4cc22
David Lancioni <dlancioni@gmail.com>
2020-09-30 14:49:41 -0300

    Ajax solved problem

--- php/test/upload1.php
+++ php/test/upload1.php
@@ -1,12 +1,27 @@
-<!DOCTYPE html>
-<html>
-<body>
+<body style="margin:0">
 
-<form action="upload2.php" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload Image" name="submit">
-</form>
+  <input type="button" value="Submit" onclick="submit()">
+  <input type="file" name="img">
 
+  <script>
+
+    async function submit() {
+
+      var input = document.querySelector('input[type="file"]')
+
+      let formData = new FormData();
+
+      formData.append("firstName", "John");
+      formData.append('file', input.files[0])
+
+      let response = await fetch('upload2.php', {
+        method: 'POST',
+        body: formData
+      });
+
+      let result = await response.json();
+      alert(result.upload);
+    }
+
+  </script>
 </body>
-</html>
