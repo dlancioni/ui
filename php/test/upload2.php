ff27eb9549332c565f99dbe438091fab80eb9ae9
David Lancioni <dlancioni@gmail.com>
2020-09-30 16:08:00 -0300

    Ajax solved problem

--- php/test/upload2.php
+++ php/test/upload2.php
@@ -1,28 +1,14 @@
 <?php
 
+  try {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["file"]["name"]);
+    $fileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    $fileSize = getimagesize($_FILES["file"]["tmp_name"]);
+    echo '{"upload":"success"}';
 
-$file = $_FILES["file"]["name"];
+  } catch (Exception $ex) {
+    echo '{"upload":"fail"}';
+  }
 
-/*
-try {
-  $target_dir = "uploads/";
-  $target_file = $target_dir . basename($_FILES["file"]["name"]);
-  $uploadOk = 1;
-  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-  // Check if image file is a actual image or fake image
-  
-    $check = getimagesize($_FILES["file"]["tmp_name"]);
-    if($check !== false) {
-      
-      echo "File is an image - " . $check["mime"] . ".";
-      $uploadOk = 1;
-    } else {
-      echo "File is not an image.";
-      $uploadOk = 0;
-    }  
-} catch (Exception $ex) {
-
-}
-*/
-  echo '{"upload":"success"}';
 ?>
