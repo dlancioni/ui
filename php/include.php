df39e8b663ec9345207211c3593d9afbd0d8b027
David Lancioni <dlancioni@gmail.com>
2020-10-08 08:36:57 -0300

    Create class folder

--- php/include.php
+++ php/include.php
@@ -3,18 +3,18 @@
     include "exception.php";
     
     // Include classes
-    include "base.php";
-    include "db.php";
-    include "sqlbuilder.php";
-    include "util.php";
-    include "report.php";
-    include "form.php";
-    include "filter.php";
-    include "element.php";
-    include "logicmenu.php";
-    include "logictable.php";
-    include "logicfield.php";
-    include "logicevent.php";
-    include "logicupload.php";
-    include "eventaction.php";
+    include "class/base.php";
+    include "class/db.php";
+    include "class/sqlbuilder.php";
+    include "class/util.php";
+    include "class/report.php";
+    include "class/form.php";
+    include "class/filter.php";
+    include "class/element.php";
+    include "class/logicmenu.php";
+    include "class/logictable.php";
+    include "class/logicfield.php";
+    include "class/logicevent.php";
+    include "class/logicupload.php";
+    include "class/eventaction.php";
 ?>    
