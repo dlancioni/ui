23920cdf8e92107bc742e2bfba885b959d90b73e
David Lancioni <dlancioni@gmail.com>
2020-10-09 13:45:24 -0300

    Doc menu

--- php/body.php
+++ php/body.php
@@ -14,8 +14,8 @@
         <ul class="navbar-nav mr-auto">       
 
             <li class="nav-item dropdown">           
-                <a class="nav-link dropdown-toggle" id="dropdown01" data-toggle="dropdown">Administração</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                <a class="nav-link dropdown-toggle" href="#" id="menu" data-toggle="dropdown">Administração</a>
+                <div class="dropdown-menu" aria-labelledby="menu">
                     <a class="dropdown-item" href="#">Action</a>
                     <a class="dropdown-item" href="#">Another action</a>
                     <a class="dropdown-item" href="#">Something else here</a>
