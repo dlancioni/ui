594e22a035379f6943a43b6a9ea12dc7cd1c93f2
David Lancioni <dlancioni@gmail.com>
2020-10-09 13:31:54 -0300

    Doc menu

--- php/body.php
+++ php/body.php
@@ -1,42 +1,53 @@
 <!-- Main container -->
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+
+    <!-- Title/Logo -->
+    <a class="navbar-brand" href="#"><b>Forms</b></a>
+    
+    <!-- Hamburger -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Menu -->
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown">           
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Administração</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+                </div>            
+            </li>
+
+            <li class="nav-item dropdown">           
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Cadastros</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+                </div>            
+            </li>          
+        </ul>
+    </div>
+
+    <!-- Topnav aligned right -->
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+            <a href="" class="text-white"><b>David</b></a>
+            </li>
+        </ul>
+    </div>
+    
+</nav>
+
+<br>
+<br>
+<br>
+
 <div class="container-fluid">
-
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-
-        <a class="navbar-brand" href="#"></a>
-        
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item dropdown">           
-                    <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Administração</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown01">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                    </div>            
-                </li>
-
-                <li class="nav-item dropdown">           
-                    <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Cadastros</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown01">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                    </div>            
-                </li>          
-            </ul>
-        </div>
-    </nav>
-
-    <br>
-    <br>
-    <br>
-
     <div class="row">
         <div class="col-sm-3">
             <?php echo $menu; ?>
