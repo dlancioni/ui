7fe5832a8d80f5ec4acad3c2d7eaa0b8bb2f9c62
David Lancioni <dlancioni@gmail.com>
2020-10-17 11:48:03 -0300

    Left side controls

--- php/formlogin.php
+++ php/formlogin.php
@@ -1,21 +1,20 @@
 <?php
 
     if (isset($_SESSION["_AUTH_"]) == false || $_SESSION["_AUTH_"] == 0) {
-        ?>    
+        ?>  
+
             <div class="form-row">
                 <div class="col">
-                    <input type="text" id="_SIGNID_" name="_SIGNID_" class="form-control form-control-sm" placeholder="Cód. Assinante" value="1">
+                    <input type="text" id="_SIGNID_" name="_SIGNID_" class="form-control" placeholder="Cód. Assinante" value="1">
                 </div>        
                 <div class="col">
-                    <input type="text" id="_USERNAME_" name="_USERNAME_" class="form-control form-control-sm" placeholder="Usuário" value="joao">
+                    <input type="text" id="_USERNAME_" name="_USERNAME_" class="form-control" placeholder="Usuário" value="joao">
                 </div>
                 <div class="col">
-                    <input type="password" id="_PASSWORD_" name="_PASSWORD_" class="form-control form-control-sm" placeholder="Senha" value="123">
+                    <input type="password" id="_PASSWORD_" name="_PASSWORD_" class="form-control" placeholder="Senha" value="123">
                 </div>
                 <div class="col">
-                    <button type="button" class="btn btn-primary btn-sm" data-toggle="button" aria-pressed="false" onclick="login()">
-                    Entrar
-                    </button>
+                    <button class="btn btn-outline-primary" onclick="login()">Login</button>
                 </div>
             </div>
         <?php
