8201d69536e8ee983e8fa67108823eb0185e2011
David Lancioni <dlancioni@gmail.com>
2020-11-13 09:28:06 -0300

    Organize classes

--- php/include.php
+++ php/include.php
@@ -3,24 +3,24 @@
     include "exception.php";
     
     // Include classes
-    include "class/base.php";
-    include "class/db.php";
-    include "class/sqlbuilder.php";
-    include "class/util.php";
-    include "class/filter.php";
-    include "class/element.php";
-    include "class/eventaction.php";    
-    include "class/model.php";
-    include "class/mail.php";
+    include "class.base.php";
+    include "class.db.php";
+    include "class.sqlbuilder.php";
+    include "class.util.php";
+    include "class.filter.php";
+    include "class.element.php";
+    include "class.eventaction.php";    
+    include "class.model.php";
+    include "class.mail.php";
     
-    include "class/logicmenu.php";
-    include "class/logictable.php";
-    include "class/logicfield.php";
-    include "class/logicevent.php";
-    include "class/logicupload.php";
-    include "class/logicauth.php";
-    include "class/logicsetup.php";
-    include "class/logicreport.php";
-    include "class/logicform.php";
+    include "class.menu.php";
+    include "class.table.php";
+    include "class.field.php";
+    include "class.event.php";
+    include "class.upload.php";
+    include "class.auth.php";
+    include "class.setup.php";
+    include "class.report.php";
+    include "class.form.php";
 
 ?>    
