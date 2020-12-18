ee0ca29250d1dc4b2e575c79d9ad4bcd5fed5bc2
David Lancioni <dlancioni@gmail.com>
2020-12-18 10:45:49 -0300

    Create hidden field ID

--- js/session.js
+++ js/session.js
@@ -1,6 +1,8 @@
-/*
+ /*
   * It manipulates hidden fields in index.htm
- */
+  */
+
+// Current module
 function getModule() {
     return field("_MODULE_").value;
 }
@@ -8,6 +10,7 @@ function setModule(value) {
     return field("_MODULE_").value = value;
 }
 
+// Current format (table or form)
 function getFormat() {
     return field("_FORMAT_").value;
 }
@@ -15,6 +18,7 @@ function setFormat(value) {
     return field("_FORMAT_").value = value;
 }
 
+// Current action (New, Edit, Delete)
 function getAction() {
     return field("_ACTION_").value;
 }
@@ -22,11 +26,20 @@ function setEvent(value) {
     return field("_ACTION_").value = value;
 }
 
+// Current page
 function getPaging() {
     return field("_PAGING_").value;
 }
 function setPaging(value) {
     return field("_PAGING_").value = value;
+}
+
+// Current or selected record
+function getId() {
+    return field("_ID_").value;
+}
+function setId(value) {
+    return field("_ID_").value = value;
 }
 
 /*
@@ -37,10 +50,11 @@ function submit(param="form1") {
     form.submit();
 }
 
-function go(module=0, format=1, event=0) {
+function go(module=0, format=1, event=0, id=0) {
     setModule(module);
     setFormat(format);
     setEvent(event);
+    setId(id);
     setPaging(0);
     submit();
 }
