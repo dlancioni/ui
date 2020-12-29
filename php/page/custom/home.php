866c265d39b91c523f7d17420394214ad8ad05f1
David Lancioni <dlancioni@gmail.com>
2020-12-29 20:23:32 -0300

    Create home page

--- php/page/custom/home.php
+++ php/page/custom/home.php
@@ -10,7 +10,7 @@
           <div class="starter-template">
             <h1>Seja bem vindo ao Forms</h1>
             <p class="lead">O unico sistema capaz de aprender o negócio da sua empresa.</p>
-            <p class="lead"><img src="img/js.png"></p>
+            <p class="lead"><img class="img-fluid" src=""></p>
           </div>
         </div>    
 
